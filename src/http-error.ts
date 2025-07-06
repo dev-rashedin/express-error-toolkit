ab564f6f3ab2b11beccebd3e38dc25cc1d1a77f8
Rashedin-063 <rashedinislam.06@gmail.com>
2025-07-06 18:28:27 +0600

    http-error function added

---
+++ src/http-error.ts
@@ -0,0 +1,9 @@
+import { CustomAPIError } from './error/custom-api-error';
+
+export function httpError(
+  statusCode: number,
+  message: string,
+  errorDetails: string | object | null = null
+): CustomAPIError {
+  return new CustomAPIError(message, statusCode, errorDetails);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 export { CustomAPIError, BadRequestError, NotFoundError, UnauthenticatedError, ConflictError, ForbiddenError, ValidationError, TooManyRequestsError } from './error'
+import { httpError } from './http-error';
 
 
 export { asyncHandler } from './async-handler'
 export { globalErrorHandler } from './global-error-handler'
 export { notFoundHandler } from './not-found-handler'
+export {httpError} from './http-error'
 
 
 export { StatusCodes, getStatusMessage } from 'http-status-toolkit'
