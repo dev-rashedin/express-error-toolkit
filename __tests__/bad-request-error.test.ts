cb671421087fcbbae62dae20a405a008fab7b200
Rashedin-063 <rashedinislam.06@gmail.com>
2025-07-06 21:56:19 +0600

    test added for bad-request-error

---
+++ __tests__/bad-request-error.test.ts
@@ -0,0 +1,19 @@
+import { isCustomAPIError } from '../src/checking-custom-api-error';
+import { BadRequestError } from '../src/error/bad-request-error';
+
+describe('isCustomAPIError()', () => {
+  it('should return true for custom errors', () => {
+    const err = new BadRequestError('Test');
+    expect(isCustomAPIError(err)).toBe(true);
+  });
+
+  it('should return false for regular errors', () => {
+    const err = new Error('Normal');
+    expect(isCustomAPIError(err)).toBe(false);
+  });
+
+  it('should return false for unknown values', () => {
+    expect(isCustomAPIError(null)).toBe(false);
+    expect(isCustomAPIError({})).toBe(false);
+  });
+});
